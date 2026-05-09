efa63fb3096249c7f33c3ffe49e466c066ed17fc
developer <henning.developer@icloud.com>
2026-05-09 21:10:51 +0200

    Update LaunchPadConfigurationExtractor.rb

--- Casks/LaunchPadConfigurationExtractor.rb
+++ Casks/LaunchPadConfigurationExtractor.rb
@@ -2,13 +2,13 @@ cask "launchpadconfigurationextractor" do
   
   # Die Version des Builds
   version "1.0.0"
-  # Das Ergebnis von 'shasum -a 256 LaunchPadConfigExtractor.dmg.dmg'
-  sha256 "b008561dc540cd7ab078cbf2e8d2b85cccea0c7e9002320788abc24bf5d52725  LaunchPadConfigurationExtractor.dmg" 
+  # Das Ergebnis von 'shasum -a 256 LaunchPadConfigurationExtractor.dmg'
+  sha256 "b008561dc540cd7ab078cbf2e8d2b85cccea0c7e9002320788abc24bf5d52725" 
   
-  url "https://github.com/krusoft-eu/homebrew-utils/releases/download/v#{version}/LaunchPadConfigExtractor.dmg"
+  url "https://github.com/krusoft-eu/homebrew-utils/releases/download/v#{version}/LaunchPadConfigurationExtractor.dmg"
   name "LaunchPadConfigExtractor"
   desc "Exports LaunchPad Configuration for migration to LaunchIt App"
-  homepage "file:///Users/developer/Documents/dev/Swift/Apps/LaunchIt/LaunchIt/Material/webseite/migration_from_launch_pad.html"
+  homepage "http://www.henning-kruse.eu/apps/launchit/migration_from_launch_pad.html"
 
   app "LaunchPadConfigExtractor.app"
 
