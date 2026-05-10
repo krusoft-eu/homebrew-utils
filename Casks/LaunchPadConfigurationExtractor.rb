5dadaf4a64c53b85c99dfa759f410168d12498b3
Henning Developer <70289895+HenningDeveloper@users.noreply.github.com>
2026-05-10 09:02:51 +0200

    Fix URL and app name in LaunchPadConfigurationExtractor
    Fix URL and app name in LaunchPadConfigurationExtractor

--- Casks/LaunchPadConfigurationExtractor.rb
+++ Casks/LaunchPadConfigurationExtractor.rb
@@ -5,11 +5,11 @@ cask "launchpadconfigurationextractor" do
   # Das Ergebnis von 'shasum -a 256 LaunchPadConfigurationExtractor.dmg'
   sha256 "b008561dc540cd7ab078cbf2e8d2b85cccea0c7e9002320788abc24bf5d52725" 
   
-  url "https://github.com/krusoft-eu/homebrew-utils/releases/download/v#{version}/LaunchPadConfigurationExtractor.dmg"
-  name "LaunchPadConfigExtractor"
+  url "https://github.com/krusoft-eu/homebrew-utils/releases/download/#{version}/LaunchPadConfigurationExtractor.dmg"
+  name "LaunchPadConfigurationExtractor"
   desc "Exports LaunchPad Configuration for migration to LaunchIt App"
   homepage "http://www.henning-kruse.eu/apps/launchit/migration_from_launch_pad.html"
 
-  app "LaunchPadConfigExtractor.app"
+  app "LaunchPadConfigurationExtractor.app"
 
 end
